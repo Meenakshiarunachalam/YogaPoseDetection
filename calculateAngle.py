bcd48256d388e3d129282bba08a02d8fe3530e41
Meenakshiarunachalam <meenakshiarunachalamm@gmail.com>
2022-11-01 12:51:45 +0530

    mediaPipe-flask Integration

---
+++ calculateAngle.py
@@ -0,0 +1,21 @@
+import math
+
+def calculateAngle(landmark1, landmark2, landmark3):
+    
+
+    # Get the required landmarks coordinates.
+    x1, y1, _ = landmark1
+    x2, y2, _ = landmark2
+    x3, y3, _ = landmark3
+
+    # Calculate the angle between the three points
+    angle = math.degrees(math.atan2(y3 - y2, x3 - x2) - math.atan2(y1 - y2, x1 - x2))
+    
+    # Check if the angle is less than zero.
+    if angle < 0:
+
+        # Add 360 to the found angle.
+        angle += 360
+    
+    # Return the calculated angle.
+    return angle
